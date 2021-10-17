dfb3cadfeceb95c53f88cec30cd59f8f9f5cc579
rade <rade@rade-Lenovo-IdeaPad-L340-15API>
2021-10-17 07:20:14 +0200

    Turn on and Turn off LEDs with Python app with Tkinter GUI

---
+++ arduino-tkinter/led.ino
@@ -0,0 +1,37 @@
+
+char userInput;
+
+#define LED 8
+#define LED1 9
+
+void setup(){
+
+  Serial.begin(9600);                        //  setup serial
+  pinMode(LED, OUTPUT);
+  pinMode(LED1, OUTPUT);
+
+}
+
+
+void loop(){
+  
+if(Serial.available()> 0){ 
+    
+    userInput = Serial.read();               // read user input
+      
+      if(userInput == '0'){                
+        digitalWrite(LED, HIGH); 
+      }
+      if(userInput == '1'){
+       digitalWrite(LED, LOW);         
+      }
+       if(userInput == '2'){                
+        digitalWrite(LED1, HIGH); 
+      }
+      if(userInput == '3'){
+       digitalWrite(LED1, LOW);         
+      }
+
+
+  } // Serial.available
+} // Void Loop
---
+++ arduino-tkinter/led.py
@@ -0,0 +1,54 @@
+import serial
+
+import tkinter as tk 
+from tkinter import *
+
+commPort = '/dev/ttyUSB0'
+ser = serial.Serial(commPort, baudrate = 9600, timeout = 1)
+
+def turnOnLED():
+    ser.write(b'0')
+
+def turnOffLED(): 
+    ser.write(b'1')
+    
+def turnOnLED1():
+    ser.write(b'2')
+
+def turnOffLED1(): 
+    ser.write(b'3')
+
+# creating tkinter window 
+root = Tk() 
+root.title('Blink GUI')
+
+btn_On1= tk.Button(root, text="Ukljuci LED", command=turnOnLED)
+btn_On1.grid(row=0, column=0)
+
+btn_Off1 = tk.Button(root, text="Iskljuci LED", command=turnOffLED)
+btn_Off1.grid(row=0, column=1)
+
+btn_On2= tk.Button(root, text="Ukljuci LED1", command=turnOnLED1)
+btn_On2.grid(row=1, column=0)
+
+btn_Off2 = tk.Button(root, text="Iskljuci LED1", command=turnOffLED1)
+btn_Off2.grid(row=1, column=1)
+
+root.geometry("350x350")
+root.mainloop()
+
+'''def turnOnLED():
+    ser.write(b'o')
+
+def turnOffLED(): 
+    ser.write(b'x')
+
+# creating tkinter window 
+root = Tk() 
+root.title('Blink GUI')
+
+btn_On= tk.Button(root, text="Turn On", command=turnOnLED)
+btn_On.grid(row=0, column=0)
+
+btn_Off = tk.Button(root, text="Turn Off", command=turnOffLED)
+btn_Off.grid(row=0, column=1)'''
