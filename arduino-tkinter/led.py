df6f8365b19415e8a9fa90dd0b9331d12beb4a33
rade <rade@rade-Lenovo-IdeaPad-L340-15API>
2021-10-17 07:54:55 +0200

    Python code

--- arduino-tkinter/led.py
+++ arduino-tkinter/led.py
@@ -36,19 +36,3 @@ btn_Off2.grid(row=1, column=1)
 
 root.geometry("350x350")
 root.mainloop()
-
-'''def turnOnLED():
-    ser.write(b'o')
-
-def turnOffLED(): 
-    ser.write(b'x')
-
-# creating tkinter window 
-root = Tk() 
-root.title('Blink GUI')
-
-btn_On= tk.Button(root, text="Turn On", command=turnOnLED)
-btn_On.grid(row=0, column=0)
-
-btn_Off = tk.Button(root, text="Turn Off", command=turnOffLED)
-btn_Off.grid(row=0, column=1)'''
